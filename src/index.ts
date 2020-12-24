587b69923e815b9fea5c089cf26be811f73fd1f2
CraftyDragon678 <craftydragon678@gmail.com>
2020-12-25 08:57:29 +0900

    feat: change handling signal logic

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,17 @@ bot.on('ready', () => {
   console.log('ready');
 });
 
-const shutdown = (signal: NodeJS.Signals) => () => {
-  console.log('bye', signal);
-  bot.destroy();
+const shutdown = (signal: NodeJS.Signals) => {
+  let done = false;
+  return (() => {
+    if (!done) {
+      done = true;
+      console.log(signal);
+      bot.destroy();
+    }
+  });
 }
 
-process.on('SIGINT', shutdown('SIGINT'));
-process.on('SIGUSR2', shutdown('SIGUSR2'));
+(<NodeJS.Signals[]>['SIGINT', 'SIGTERM', 'SIGUSR2']).forEach((signal) => {
+  process.on(signal, shutdown(signal));
+});
